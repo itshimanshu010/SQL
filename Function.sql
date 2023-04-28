248236816a3b083a364f9db521b00ab049a06a95
Himanshu Jaiswal <55348957+itshimanshu010@users.noreply.github.com>
2023-04-28 14:34:58 +0530

    Create Function.sql

---
+++ Function.sql
@@ -0,0 +1,10 @@
+SELECT lower('DDDDDDDD') "Lower Case",upper('himan'),INITCAP('dsfs sggs') from dual 
+SELECT abs(-11) from dual
+SELECT ROUND(11.6457474,2) from dual
+SELECT ceil(11.645747) from dual
+SELECT floor(11.6),floor(-11.6)from dual
+SELECT LENGTH('Jai Jo') from dual
+SELECT CONCAT('fff', 'yyyy') from dual
+SELECT SUBSTR('Ashutosh Kutta',3,5) from dual
+SELECT SUBSTR('Ashutosh Kutta',-3,17) from dual
+SELECT to_char(94,99.99) from dual
